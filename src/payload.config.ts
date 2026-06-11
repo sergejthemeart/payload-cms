c4dfac64658b60bac2f4e7091af9229bc877ba0f
root <root@v2202606293714465118.nicesrv.de>
2026-06-11 09:42:50 +0200

    feat: set German as default admin language

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -20,6 +20,9 @@ const dirname = path.dirname(filename)
 
 export default buildConfig({
   admin: {
+    language: {
+      defaultLanguage: 'de',
+    },
     components: {
       // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
       // Feel free to delete this at any time. Simply remove the line below.
