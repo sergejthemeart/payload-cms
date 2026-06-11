ec83e6873801f3a295596de909ab64c94ba2208a
root <root@v2202606293714465118.nicesrv.de>
2026-06-11 09:30:53 +0200

    feat: add initial payload migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260611_073013 from './20260611_073013';
+
+export const migrations = [
+  {
+    up: migration_20260611_073013.up,
+    down: migration_20260611_073013.down,
+    name: '20260611_073013'
+  },
+];
